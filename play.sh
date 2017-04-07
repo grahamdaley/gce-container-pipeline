d3118f1c37e553fb7d190b9f5a9c58849a01080c
Graham Daley <gdaley@palo-it.com>
2017-04-07 10:30:25 +0800

    Changed to install Jenkins using apt instead of Docker

--- play.sh
+++ play.sh
@@ -13,4 +13,4 @@ if [[ ! -f "$SSL_CERT_FILE" ]]; then
   curl -O http://curl.haxx.se/ca/cacert.pem
 fi
 
-ansible-playbook "$PLAYBOOK"
+ansible-playbook -vvv "$PLAYBOOK"
--- roles/get-ssl/templates/getssl.sh
+++ roles/get-ssl/templates/getssl.sh
@@ -6,22 +6,13 @@
 #
 FQDN="{{ machine_names }}-{{ project_id }}.{{ project_domain }}"
 CERT_SRC_DIR="/etc/letsencrypt/live/${FQDN}"
-CERT_DEST_DIR="{{ jenkins_home }}/ssl"
-
-copy2jenkins () {
-	mkdir -p ${CERT_DEST_DIR}
-	/usr/bin/openssl rsa -in "${CERT_SRC_DIR}/privkey.pem" -out "${CERT_DEST_DIR}/privkey-rsa.pem"
-	cp "${CERT_SRC_DIR}/fullchain.pem" ${CERT_DEST_DIR}
-}
 
 if [ -d "$CERT_SRC_DIR" ]; then
 	/usr/bin/certbot renew --quiet >> /var/log/le-renew.log
    	if [ "$?" -eq 1 ]; then
-		copy2jenkins
-		docker stop jenkins
-		docker start jenkins
+   		# An updated cert was received, so restart nginx
+   		systemctl restart nginx
    	fi
 else
    	/usr/bin/certbot certonly --standalone --agree-tos --email "{{ ssl_email }}" -d "${FQDN}"
-	copy2jenkins
 fi
